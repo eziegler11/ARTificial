c881f5267e373cb8a2a512efb88fbaf57691dad3
erichziegler <erich.ziegler11@gmail.com>
2023-08-17 17:45:52 -0600

    Added ability to fill in form and use Surprise Me button

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,13 +15,20 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {}
+  const generateImage = () => {
+    
+  }
 
   const handleSubmit = () => {}
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
 
-  const handleSurpriseMe = () => {}
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
+  }
 
   return (
 		<section className='max-w-7xl mx-auto'>
